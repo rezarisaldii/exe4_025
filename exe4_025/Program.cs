21768cb6145755d54c0e0de29a5f8b9a94146621
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-12-09 15:55:19 +0700

    declare the size of the stack

--- exe4_025/Program.cs
+++ exe4_025/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace exe4_025
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            int top = -1;
+            int[] reza = new int[45];
+            Console.WriteLine("Enter Size Of The Stack");
+            int MAX = Convert.ToInt32(Console.ReadLine());
+            while(true)
+            {
+
+            }
         }
     }
 }
